9339f3435351b2a47e85dc9c09e8eac4996110f4
Martijn <martijnfs@me.com>
2019-11-27 13:22:54 +0100

    First Commit

---
+++ player.py
@@ -0,0 +1,19 @@
+class player:
+
+    def __init__ (self, name, cannons, coins, isalive):
+        self.name = name
+        self.cannons = cannons
+        self.coins = coins
+        self.isalive = isalive
+
+    def playerDeath(self):
+        player.isalive = False
+
+    def calculateScore(self):
+        self.score = self.cannons + self.munten
+
+    def loseCannon(self):
+        player.cannons -= 1
+
+    def loseCoin(self):
+        player.coins -= 1
